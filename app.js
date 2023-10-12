7c9ceee5f517298357cec74d7e52d0fa459b5528
Lucas Busso <lucasxbusso@gmail.com>
2023-10-12 19:21:49 -0300

    initial commit. starting serve

---
+++ app.js
@@ -0,0 +1,10 @@
+require("dotenv").config();
+const express = require("express");
+const cors = require("cors");
+const PORT = process.env.PORT | 3000;
+
+const app = express();
+app.use(cors());
+app.listen(PORT, () => {
+  console.log(`Up and running on port ${PORT}`);
+});
