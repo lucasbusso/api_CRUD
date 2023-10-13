86b8023f38f10cdc08d864fdfc1f5aed13146513
Lucas Busso <lucasxbusso@gmail.com>
2023-10-13 20:38:14 -0300

    all track controllers

--- controllers/tracks.controller.js
+++ controllers/tracks.controller.js
@@ -1,18 +1,55 @@
+const { matchedData } = require("express-validator");
 const { tracksModel } = require("../models");
+const handleHttpError = require("../utils/handleError");
 
 const getItems = async (req, res) => {
-  const data = await tracksModel.find({});
-  res.send({ data });
+  try {
+    const data = await tracksModel.find({});
+    res.send({ data });
+  } catch (error) {
+    handleHttpError(res, "ERROR_GET_ITEMS");
+  }
 };
-const getItem = (req, res) => {};
+const getItem = async (req, res) => {
+  try {
+    req = matchedData(req);
+    const { id } = req;
+    const data = await tracksModel.findById(id);
+    res.send({ data });
+  } catch (error) {
+    handleHttpError(res, "ERROR_GET_ITEM");
+  }
+};
 const createItem = async (req, res) => {
-  const { body } = req;
-  const data = await tracksModel.create(body);
-  res.send({ data });
-  console.log(data);
+  try {
+    const body = matchedData(req);
+    const data = await tracksModel.create(body);
+    res.send({ data });
+  } catch (error) {
+    handleHttpError(res, "ERROR_CREATE_ITEM");
+  }
 };
-const updateItem = (req, res) => {};
-const deleteItem = (req, res) => {};
+const updateItem = async (req, res) => {
+  try {
+    const { id, ...body } = matchedData(req);
+    const data = await tracksModel.findOneAndUpdate({ id }, body);
+    res.send({ data });
+  } catch (error) {
+    console.log(error);
+    handleHttpError(res, "ERROR_UPDATE_ITEM");
+  }
+};
+const deleteItem = async (req, res) => {
+  try {
+    req = matchedData(req);
+    const { id } = req;
+    const data = await tracksModel.deleteOne({ _id: id });
+    res.send({ data });
+  } catch (error) {
+    console.log(error);
+    handleHttpError(res, "ERROR_DELETE_ITEM");
+  }
+};
 
 module.exports = {
   getItems,
