1fd70638c3bd242bf612481b1addb4749437924e
Lucas Busso <lucasxbusso@gmail.com>
2023-10-13 18:06:11 -0300

    track routes create item validation

---
+++ utils/validator/track.js
@@ -0,0 +1,19 @@
+const { check } = require("express-validator");
+const validateResult = require("../handleValidator");
+
+const validatorCreateItem = [
+  check("name").notEmpty().exists(),
+  check("album").notEmpty().exists(),
+  check("cover").notEmpty().exists(),
+  check("artist").notEmpty().exists(),
+  check("artist.name").notEmpty().exists(),
+  check("artist.nickname").notEmpty().exists(),
+  check("artist.nationality").notEmpty().exists(),
+  check("duration").notEmpty().exists(),
+  check("duration.start").notEmpty().exists(),
+  check("duration.end").notEmpty().exists(),
+  check("mediaId").notEmpty().exists().isMongoId(),
+  (req, res, next) => validateResult(req, res, next),
+];
+
+module.exports = { validatorCreateItem };
