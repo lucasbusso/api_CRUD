25ee8ba34f0ab37bb4b76ff536511a7559631225
Lucas Busso <lucasxbusso@gmail.com>
2023-10-13 13:54:55 -0300

    track route and server updates

---
+++ routes/tracks.js
@@ -0,0 +1,13 @@
+const express = require("express");
+const {
+  getItems,
+  getItem,
+  createItem,
+} = require("../controllers/tracks.controller");
+const router = express.Router();
+
+router.get("/", getItems);
+router.get("/:id", getItem);
+router.post("/", createItem);
+
+module.exports = router;
