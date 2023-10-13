4cbb8c8e7b67422046383f0c9ddc066185a3f4a5
Lucas Busso <lucasxbusso@gmail.com>
2023-10-13 15:53:58 -0300

    app server refactor and static folder usage

--- app.js
+++ app.js
@@ -2,13 +2,18 @@ require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
 const dbConnection = require("./config/mongo");
+
 const PORT = process.env.PORT | 3000;
 
 const app = express();
+
 app.use(cors());
 app.use(express.json());
 app.use("/api", require("./routes"));
+app.use(express.static("storage"));
+
 app.listen(PORT, () => {
   console.log(`Up and running on port ${PORT}`);
 });
+
 dbConnection();
