da604d6bbf5505fb42452afbeb4f972b0da8eb08
Lucas Busso <lucasxbusso@gmail.com>
2023-10-13 20:37:36 -0300

    all track routes

--- routes/tracks.js
+++ routes/tracks.js
@@ -3,13 +3,20 @@ const {
   getItems,
   getItem,
   createItem,
+  updateItem,
+  deleteItem,
 } = require("../controllers/tracks.controller");
-const { validatorCreateItem } = require("../utils/validator/track");
+const {
+  validatorCreateItem,
+  validatorGetItem,
+} = require("../utils/validator/track");
 
 const router = express.Router();
 
 router.get("/", getItems);
-router.get("/:id", getItem);
+router.get("/:id", validatorGetItem, getItem);
 router.post("/", validatorCreateItem, createItem);
+router.put("/:id", validatorGetItem, validatorCreateItem, updateItem);
+router.delete("/:id", validatorGetItem, deleteItem);
 
 module.exports = router;
