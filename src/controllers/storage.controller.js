8e5448ae30dea5ce5856e950951855206fdae037
Lucas Busso <lucasxbusso@gmail.com>
2023-10-26 17:48:01 -0300

    storage service decoupled from controller

--- src/controllers/storage.controller.js
+++ src/controllers/storage.controller.js
@@ -1,68 +1,62 @@
-const fs = require("fs");
 const { matchedData } = require("express-validator");
-const { storageModel } = require("../models");
 const handleHttpError = require("../utils/handleError");
+const {
+  getStorageListService,
+  getStorageDetailsService,
+  createStorageRecordService,
+  deleteStorageRecordService,
+} = require("../services/storage.service");
 
 const PUBLIC_URL = process.env.PUBLIC_URL;
-const MEDIA_PATH = `${__dirname}/../storage`;
 
-const getItems = async (req, res) => {
+const getStorageList = async (req, res) => {
   try {
-    const data = await storageModel.find({});
-    const length = data.length;
+    const { length, data } = await getStorageListService(res);
     res.status(200).send({ length, data });
   } catch (error) {
     handleHttpError(res, "ERROR_GET_ITEMS");
   }
 };
 
-const getItem = async (req, res) => {
+const getStorageDetail = async (req, res) => {
   try {
     const { id } = matchedData(req);
-    const data = await storageModel.findById(id);
-    res.status(200).send({ data });
+    const data = await getStorageDetailsService(id, res);
+    res.status(200).send(data);
   } catch (error) {
     handleHttpError(res, "ERROR_GET_ITEM");
   }
 };
 
-const createItem = async (req, res) => {
+const createStorageRecord = async (req, res) => {
   try {
     const { file } = req;
     const body = {
       url: `${PUBLIC_URL}/${file.filename}`,
       filename: file.filename,
     };
-    const response = await storageModel.create(body);
-    res.status(201).send({ response });
-  } catch (e) {
+    const response = await createStorageRecordService(body, res);
+    res.status(201).send(response);
+  } catch (error) {
+    console.log({ error });
     handleHttpError(res, "ERROR_UPLOAD_ITEM");
   }
 };
 
-const deleteItem = async (req, res) => {
+const deleteStorageRecord = async (req, res) => {
   try {
     req = matchedData(req);
     const id = req.id;
-    const findMedia = await storageModel.findById(id);
-    const fileName = findMedia.filename;
-    await storageModel.delete({ _id: id });
-    fs.unlinkSync(`${MEDIA_PATH}/${fileName}`);
-
-    const data = {
-      findMedia: fileName,
-      deleted: true,
-    };
-
-    res.status(201).send({ data });
+    const data = await deleteStorageRecordService(id, res);
+    res.status(201).send(data);
   } catch (error) {
     handleHttpError(res, "ERROR_DELETE_ITEM");
   }
 };
 
 module.exports = {
-  getItems,
-  getItem,
-  createItem,
-  deleteItem,
+  getStorageList,
+  getStorageDetail,
+  createStorageRecord,
+  deleteStorageRecord,
 };
